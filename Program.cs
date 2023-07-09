167b4f5bcc6e0be6a23de888f88a6773956f72cf
grabovskij.l <leonidgrabovskij941@gmail.com>
2023-07-09 16:35:41 +0300

    added map example

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Example_1_classes
 {
@@ -26,29 +29,46 @@ namespace Example_1_classes
     {
         public static void Main(string[] args)
         {
-            dynamic car1 = new Car("red",2);
-            var car2 = new Car("blue",4);
+            List<Car> cars = new List<Car>();
 
-            car1 = new Car("yellow", 3);
-            car1 = "";
+            cars.Add(new Car("red", 2));
+            cars.Add(new Car("yellow", 3));
 
+            Console.WriteLine($"Спущено машин с конвеера:{Car.count}");
 
-            
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.toString());
+            }
 
-            if (car1 is Car) {
-                Console.WriteLine((car1 as Car).toString());
+            Dictionary<String, Car> namedCars = new Dictionary<String, Car>();
+
+            const String olegsCar = "Oleg's car";
+            const String vladimirsCar = "Vladimir's car";
+
+            namedCars.Add(olegsCar, new Car("yellow", 3));
+            namedCars.Add(vladimirsCar, new Car("red", 1));
+
+            if (namedCars.ContainsKey("olegsCar"))
+            {
+                Console.WriteLine($"{olegsCar}:{namedCars["olegsCar"].toString()}");
             }
 
 
-            Console.WriteLine(car2.toString());
-            Console.WriteLine($"Спущено машин с конвеера:{Car.count}");
+            // car1 = new Car("yellow", 3);
+            // car1 = "";
+            //
+            //
+            // if (car1 is Car)
+            // {
+            //     Console.WriteLine((car1 as Car).toString());
+            // }
+
+
+            // Console.WriteLine(car2.toString());
+            // Console.WriteLine($"Спущено машин с конвеера:{Car.count}");
 
             Console.ReadLine();
         }
     }
-
 }
-
-
-
-
